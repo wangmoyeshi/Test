0f59b13bc68fc14c4770a434655f9336104205c0
yeshiwangmo <02230118.cst@rub.edu.bt>
2024-02-29 23:28:57 +0600

    second commit

---
+++ activity.py
@@ -0,0 +1,10 @@
+name = input("enter your name:")
+num_1 = float(input("enter number 1:"))
+num_2 = float(input("enter number 2:"))
+
+print(num_1 + num_2)
+print(num_1 - num_2)
+print(num_1 * num_2)
+print(num_1 % num_2)
+
+print(f"hello {name}")
