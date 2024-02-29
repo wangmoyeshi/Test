6c91be32929aaf86780f163c17cb969e9fab12db
yeshiwangmo <02230118.cst@rub.edu.bt>
2024-02-29 23:24:34 +0600

    first commit

---
+++ secondpython.py
@@ -0,0 +1,11 @@
+grade = 85
+if grade >= 90:
+    print("Grade: A")
+elif grade >= 80:
+    print("Grade: B")
+elif grade >= 70:
+    print("Grade: C")
+elif grade >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
