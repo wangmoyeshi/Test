286ddd4625ea91167703af19d966e0b5411d9453
yeshiwangmo <02230118.cst@rub.edu.bt>
2024-05-27 23:21:25 +0600

    climb

---
+++ climbingstairs.py
@@ -0,0 +1,16 @@
+class solution(object):
+    def climbStairs(self, n):
+        memo = {}
+        return self.ways(n, memo)
+
+    def ways(self, n, memo):
+        if n in memo:
+            return memo[n]
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        
+        memo[n] = self.ways(n - 1, memo) + self.ways(n - 2, memo)
+        return memo[n]
+        
