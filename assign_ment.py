953efc50ba411bad869be612b431cbc0a456ad1b
yeshiwangmo <02230118.cst@rub.edu.bt>
2024-05-27 23:00:03 +0600

    assign

---
+++ assign_ment.py
@@ -0,0 +1,9 @@
+age = int(input("enter your age:"))
+status = input("are you a student?(yes or no):")
+
+if age <= 12:
+    print("you are eligible for a discount on the movie ticket")
+elif 13 <= age <= 18 and status == "yes":
+    print("you are eligile for a discount on the movie ticket")
+else:
+    print("you are not eligible for discount")
