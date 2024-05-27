ac33a815263b9fdef8c520f255b19142e119b6ec
yeshiwangmo <02230118.cst@rub.edu.bt>
2024-05-27 23:32:02 +0600

    object

---
+++ object_attribute.py
@@ -0,0 +1,19 @@
+class Dog:
+    def __init__(self, name, colour, age):
+        self.name = name
+        self.colour = colour
+        self.age = age
+
+dawa = Dog("dawa", "blue", 23)
+karma = Dog("karma","brown", 2)
+blacky = Dog("blacky","black", 15)
+
+print(dawa.name)
+print(dawa.colour)
+print(dawa.age)
+print(karma.name)
+print(karma.colour)
+print(karma.age)
+print(blacky.name)
+print(blacky.colour)
+print(blacky.age)
